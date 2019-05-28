916e7b478671e5de848e4ab654bd4dacc5a453e8
anw0514 <anw0514@gmail.com>
2019-05-28 07:16:42 -0400

    semantic menu component

--- src/navigation/Navbar.js
+++ src/navigation/Navbar.js
@@ -1,8 +1,15 @@
 import React, {Component} from 'react'
+import { Menu } from 'semantic-ui-react';
 
 class Navbar extends Component {
     render() {
-        return <div>this is the nav</div>
+        return (
+            <Menu>
+                <Menu.Item>Option 1</Menu.Item>
+                <Menu.Item>Option 2</Menu.Item>
+                <Menu.Item>Option 3</Menu.Item>
+            </Menu>
+        )
     }
 }
 
