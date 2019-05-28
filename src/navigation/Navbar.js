2d16193109f88077684b56674981a93751cefea9
anw0514 <anw0514@gmail.com>
2019-05-28 07:18:01 -0400

    nav state

--- src/navigation/Navbar.js
+++ src/navigation/Navbar.js
@@ -2,12 +2,20 @@ import React, {Component} from 'react'
 import { Menu } from 'semantic-ui-react';
 
 class Navbar extends Component {
+
+    constructor() {
+        super()
+        this.state = {
+            active: ''
+        }
+    }
+
     render() {
         return (
             <Menu>
-                <Menu.Item>Option 1</Menu.Item>
-                <Menu.Item>Option 2</Menu.Item>
-                <Menu.Item>Option 3</Menu.Item>
+                <Menu.Item active={this.state.active === 'Option 1'}>Option 1</Menu.Item>
+                <Menu.Item active={this.state.active === 'Option 2'}>Option 2</Menu.Item>
+                <Menu.Item active={this.state.active === 'Option 3'}>Option 3</Menu.Item>
             </Menu>
         )
     }
