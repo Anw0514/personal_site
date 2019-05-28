753c16b2e68139924a59f8fe90d7105e4043faca
anw0514 <anw0514@gmail.com>
2019-05-28 07:24:34 -0400

    active nav item changes on click

--- src/navigation/Navbar.js
+++ src/navigation/Navbar.js
@@ -6,16 +6,35 @@ class Navbar extends Component {
     constructor() {
         super()
         this.state = {
-            active: ''
+            activeItem: ''
         }
+    }
+
+    changeActiveItem = (string) => {
+        this.setState({activeItem: string})
     }
 
     render() {
         return (
             <Menu>
-                <Menu.Item active={this.state.active === 'Option 1'}>Option 1</Menu.Item>
-                <Menu.Item active={this.state.active === 'Option 2'}>Option 2</Menu.Item>
-                <Menu.Item active={this.state.active === 'Option 3'}>Option 3</Menu.Item>
+                <Menu.Item 
+                    active={this.state.activeItem === 'Option 1'}
+                    onClick={() => this.changeActiveItem('Option 1')}
+                >
+                    Option 1
+                </Menu.Item>
+                <Menu.Item 
+                    active={this.state.activeItem === 'Option 2'}
+                    onClick={() => this.changeActiveItem('Option 2')}
+                >
+                    Option 2
+                </Menu.Item>
+                <Menu.Item 
+                    active={this.state.activeItem === 'Option 3'}
+                    onClick={() => this.changeActiveItem('Option 3')}
+                >
+                    Option 3
+                </Menu.Item>
             </Menu>
         )
     }
