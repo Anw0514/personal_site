4113a58a8cc830d1e6c2581774147f131ad53567
anw0514 <anw0514@gmail.com>
2019-05-28 06:18:11 -0400

    basic navbar and content components

---
+++ src/containers/PageSection.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class PageSection extends Component {
+    render() {
+        return <div className='App'>This is the content</div>
+    }
+}
+
+export default PageSection
---
+++ src/navigation/Navbar.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class Navbar extends Component {
+    render() {
+        return <div>this is the nav</div>
+    }
+}
+
+export default Navbar
