251fe41e9daa010965fcf51fc1c0af3159b6244b
anw0514 <anw0514@gmail.com>
2019-05-28 11:58:36 -0400

    figuring out slanted divs

--- src/containers/PageSection.js
+++ src/containers/PageSection.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react'
 
 class PageSection extends Component {
     render() {
-        return <div className='App'>This is the content</div>
+        return (
+            <div className='App'>
+            <div className='TestDiv1'>
+                <div className='sub-text'>This text should come up normally</div>
+            </div>
+            <div className='TestDiv2'></div>
+            </div>
+        )
     }
 }
 
